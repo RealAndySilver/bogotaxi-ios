ba0968c32fe3f0a71c2b6a3c62637a5068789161
Andres Abril <andres.abril@gmail.com>
2012-11-22 10:59:55 -0500

    Subiendo codigo base

---
+++ BogoTaxi/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BogoTaxi
+//
+//  Created by Andres Abril on 22/11/12.
+//  Copyright (c) 2012 Andres Abril. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
